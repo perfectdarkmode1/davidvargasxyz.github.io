4abaef32f6b92b1055ba2a7059f8271669eea1aa
perfectdarkmode1 <tdavetech@gmail.com>
2024-06-21 05:42:21 -0700

    commit

--- data/headerNavLinks.ts
+++
@@ -1,8 +0,0 @@
-const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/now', title: 'Now' },
-  { href: '/about', title: 'About' },
-  { href: '/booknotes', title: 'Book Notes' },
-]
-
-export default headerNavLinks
---
+++ docs/index.search.js
@@ -0,0 +1,58 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "",
+    "content": "RHCSA Stuff RHCSA Notes RHCSA Study Tracker Redhat Exam Environment How to Study for RHCSA RHCSA Study Points RHCSA Vagrant Lab Setup Fedora My Fedora Setup Misc Using Man Pages wsl2 Labs Turn a file into a block device ",
+    "description": "RHCSA Stuff RHCSA Notes RHCSA Study Tracker Redhat Exam Environment How to Study for RHCSA RHCSA Study Points RHCSA Vagrant Lab Setup Fedora My Fedora Setup Misc Using Man Pages wsl2 Labs Turn a file into a block device ",
+    "tags": [],
+    "title": "Linux",
+    "uri": "/linux/"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Articles ccna_spaced_repetition resources_for_passing_ccna things_to_learn_after_ccna The formatting and images of all of my networking notes go destroyed when migrating away from OneNote. But they still come in handy all of the time.\nSee my networking notes:\nCisco ccnp_devnet_sandbox_setup cisco_vlan_lab cli cml_setup_in_vmware device_management_protocols dhcp_config dhcp_snooping_and_arp_inspection ethernet_lans extended_acls ip_services ipv6 ipv6_addressing_and_subnetting ipv6_config ipv6_routing l2-etherchannel lan_architecture management nat ospf_concepts ospf_config ospf_lab ospf_network_types_and_neighbors qos router_operation routing_in_the_lan rstp securing_network_devices security_architectures spanning_tree spanning_tree_protocol_concepts standard_acls static_routes switch_interfaces switchport_security_config t-shoot_routing tcp_ip_basic tcp_ip_transport_and_applications vlans wan_architecture wans_and_ip_routing what_is_a_vlan_ wireless wireless_architectures wireless_lan wireless_security Juniper cisco_to_junos_cheat_sheet consoling_in_to_mx80_from_linux juniper_cli_basics junos_fundementals junos_paste_config toggle_poe ",
+    "description": "Articles ccna_spaced_repetition resources_for_passing_ccna things_to_learn_after_ccna The formatting and images of all of my networking notes go destroyed when migrating away from OneNote. But they still come in handy all of the time.\nSee my networking notes:\nCisco ccnp_devnet_sandbox_setup cisco_vlan_lab cli cml_setup_in_vmware device_management_protocols dhcp_config dhcp_snooping_and_arp_inspection ethernet_lans extended_acls ip_services ipv6 ipv6_addressing_and_subnetting ipv6_config ipv6_routing l2-etherchannel lan_architecture management nat ospf_concepts ospf_config ospf_lab ospf_network_types_and_neighbors qos router_operation routing_in_the_lan rstp securing_network_devices security_architectures spanning_tree spanning_tree_protocol_concepts standard_acls static_routes switch_interfaces switchport_security_config t-shoot_routing tcp_ip_basic tcp_ip_transport_and_applications vlans wan_architecture wans_and_ip_routing what_is_a_vlan_ wireless wireless_architectures wireless_lan wireless_security Juniper cisco_to_junos_cheat_sheet consoling_in_to_mx80_from_linux juniper_cli_basics junos_fundementals junos_paste_config toggle_poe ",
+    "tags": [],
+    "title": "Networking",
+    "uri": "/networking/"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Containers Calibre Web with docker distrobox vagrantguide Website Apache buildawebsite githubpageshugo hugorelearn SCSSandSASSinhugo Add Plausible to Hugo Site Obsidian Free Obsidian sync Building a Dashboard How to use Templates How to use the dataview plugin YAML In Obsidian image_to_text Tracker Plugin Setup Git gitguide Vim vimguide Vim mode in bash Misc Calibre_Server emailserver iphonemigrate nextcloud pandoc Process-bookfusion-highlights proxmox_setup rsync Serial-in-on-linux Silverblue tmuxbook VS_Codium LEGO_BRICK_LABELS-CONTACT_SHEET Upcoming Projects: Searx Create my own searx search engine\ngo to https://searx.space/\nRSS Create and RSS feed of my site and subscribe to other feeds.\nFollow these sites:\nhttps://denshi.org/ https://drewdevault.com/ https://opensourcemusings.com/support https://www.mrmoneymustache.com/\nDWM Tile Manager http://thedarnedestthing.com/vim/wiki%20cheatsheet\n[[../Security/firefox_hardening]]\nBrowser Sync https://www.xbrowsersync.org/\nPrivacy tools https://www.privacytools.io/",
+    "description": "Containers Calibre Web with docker distrobox vagrantguide Website Apache buildawebsite githubpageshugo hugorelearn SCSSandSASSinhugo Add Plausible to Hugo Site Obsidian Free Obsidian sync Building a Dashboard How to use Templates How to use the dataview plugin YAML In Obsidian image_to_text Tracker Plugin Setup Git gitguide Vim vimguide Vim mode in bash Misc Calibre_Server emailserver iphonemigrate nextcloud pandoc Process-bookfusion-highlights proxmox_setup rsync Serial-in-on-linux Silverblue tmuxbook VS_Codium LEGO_BRICK_LABELS-CONTACT_SHEET Upcoming Projects: Searx Create my own searx search engine",
+    "tags": [],
+    "title": "Tools",
+    "uri": "/tools/"
+  },
+  {
+    "breadcrumb": "",
+    "content": "I wanted a category to share what I learn about writing. So i can improve my writing skills and become a better thinker and better at helping others.\nThings I Learned from Dickie Bush and Nicolas Cole’s Writing Email Course ",
+    "description": "I wanted a category to share what I learn about writing. So i can improve my writing skills and become a better thinker and better at helping others.\nThings I Learned from Dickie Bush and Nicolas Cole’s Writing Email Course ",
+    "tags": [],
+    "title": "Writing",
+    "uri": "/writing/"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/"
+  },
+  {
+    "breadcrumb": "Python",
+    "content": "Expressions consist of values (such as 2) and operators (such as +), and they can always evaluate (that is, reduce) down to a single value.\nA single value with no operators is also considered an expression, though it evaluates only to itself, as shown here:\n2\n2\nTable 1-1: Math Operators from Highest to Lowest Precedence\nOperator\nOperation\nExample\nEvaluates to . . .\n**\nExponent\n2 ** 3\n8\n%\nModulus/remainder\n22 % 8\n6\n//\nInteger division/floored quotient\n22 // 8\n2\n/\nDivision\n22 / 8\n2.75\nMultiplication\n3 * 5\n15\nSubtraction\n5 - 2\n3\nAddition\n2 + 2\n4\nWhitespace in between the operators and values doesn’t matter for Python (except for the indentation at the beginning of the line), but a single space is convention",
+    "description": "Expressions consist of values (such as 2) and operators (such as +), and they can always evaluate (that is, reduce) down to a single value.\nA single value with no operators is also considered an expression, though it evaluates only to itself, as shown here:\n2\n2\nTable 1-1: Math Operators from Highest to Lowest Precedence\nOperator\nOperation\nExample\nEvaluates to . . .\n**\nExponent\n2 ** 3\n8",
+    "tags": [],
+    "title": "Python Basics",
+    "uri": "/python/basics/"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/"
+  }
+]
