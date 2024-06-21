81e1a77e71c9c0789679f3587fc01e0de27a874e
perfectdarkmode1 <tdavetech@gmail.com>
2024-06-21 11:47:32 -0700

    commit

---
+++ docs/pir/index.search.js
@@ -0,0 +1,18 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categorrries",
+    "uri": "/pir/categories/"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Taks",
+    "uri": "/pir/tags/"
+  }
+]
